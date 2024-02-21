2b5a51bca43b9de78fb0a5c824d8c944f1023fa1
Muhammadreza Haghiri <haghiri75@gmail.com>
2024-02-21 13:28:37 +0330

    made it more flexible

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const { Sequelize } = require('sequelize');
 const path = require('path');
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './db/database.sqlite'
+});
