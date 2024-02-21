3aa0763ef32372108b8c41355a3996ae0de90ae5
Muhammadreza Haghiri <haghiri75@gmail.com>
2024-02-21 13:29:54 +0330

    using public

--- server.js
+++ server.js
@@ -12,3 +12,5 @@ const PORT = process.env.PORT || 8000;
 
 app.set('view engine', 'ejs'); // Using EJS as the template engine
 app.set('views', path.join(__dirname, 'views')); // Path to views folder
+
+app.use(express.static(path.join(__dirname, 'public')));
