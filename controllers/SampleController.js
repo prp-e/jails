f8c7adb9c5c4ef2f9f0bd4b65e769b6ea5a1fae0
Muhammadreza Haghiri <haghiri75@gmail.com>
2024-02-21 13:58:35 +0330

    made a sample controller

--- controllers/SampleController.js
+++ controllers/SampleController.js
@@ -1,6 +1,11 @@
 const SampleController = {
     getIndex: (req, res) => {
-        
+        try {
+            res.render('sample'); 
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Internal Server Error');
+        }
     }
 
 }
